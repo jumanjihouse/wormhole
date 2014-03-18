d28e0fde1d7f01d267fdc1ef5ae4dc985e826f92
Paul Morgan <jumanjiman@gmail.com>
2014-03-18 16:06:43 +0000

    add script to restore user data container from backup

---
+++ restore.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+user=$1
+if test -z $user; then
+  echo "Usage: $(basename $0) <username>" 2> /dev/null
+  exit 1
+fi
+
+docker run -v /home/$user -name $user-data busybox true
+docker run -rm --volumes-from $user-data -v $(pwd):/backup busybox tar xvf /backup/$user-data.tar
