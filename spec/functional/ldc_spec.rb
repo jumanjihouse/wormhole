529d0675c5f638bf487ae38861f78ba08b930b3f
Paul Morgan <jumanjiman@gmail.com>
2014-12-20 21:14:56 +0000

    BZ1176361: do not run functional test against ldc2
    Add test.sh script to misc/ directory to demonstrate breakage.
    Output of test.sh:
    
    ===================
    SUMMARY for Sat Dec 20 21:59:21 UTC 2014
    -------------------
    Fedora 20 (Heisenbug) branch master FAILED to compile
    Fedora 20 (Heisenbug) branch heads/0.4 compiles OK and runs OK
    Fedora 21 (Twenty One) branch master compiles OK but FAILS to run
    Fedora 21 (Twenty One) branch heads/0.4 compiles OK but FAILS to run
    Ubuntu 14.04.1 LTS, Trusty Tahr branch master compiles OK and runs OK
    Ubuntu 14.04.1 LTS, Trusty Tahr branch heads/0.4 compiles OK and runs OK

--- spec/functional/ldc_spec.rb
+++ spec/functional/ldc_spec.rb
@@ -11,6 +11,8 @@ describe 'ldc D compiler' do
   # - ldc2 compiler
   # - git
   it 'compiles a D program' do
+    pending 'https://bugzilla.redhat.com/show_bug.cgi?id=1176361'
+
     # rubocop:disable LineLength
     cmd = 'cd dhcptest; ldc2 dhcptest.d; chmod 0755 dhcptest; ./dhcptest -h 2>&1'
     # rubocop:enable LineLength
